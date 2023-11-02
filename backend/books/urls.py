aac3941e706090032624c8373eacd57c822b3b26
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2023-11-01 19:06:47 -0600

    Links on the books to their details added

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -3,8 +3,9 @@ from .views import book_title_list_view, book_title_detail_view
 
 app_name = 'books'
 
+
 urlpatterns = [
     # Comes from books/views.py | http://127.0.0.1:8000/books/1/ example
-    path('', book_title_list_view),
-    path('<pk>/', book_title_detail_view), # <pk> - primary key, <str:text> - string, <slug> - slug
+    path('', book_title_list_view, name='main'),
+    path('<pk>/', book_title_detail_view, name='detail'), # <pk> - primary key, <str:text> - string, <slug> - slug
 ]
