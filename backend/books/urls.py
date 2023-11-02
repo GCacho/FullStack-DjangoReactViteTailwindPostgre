668e25c8647dad29b11558ff3d5327d5aff0fcb8
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2023-11-01 19:00:52 -0600

    Added missing folders and files

---
+++ backend/books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import book_title_list_view, book_title_detail_view
+
+app_name = 'books'
+
+urlpatterns = [
+    # Comes from books/views.py | http://127.0.0.1:8000/books/1/ example
+    path('', book_title_list_view),
+    path('<pk>/', book_title_detail_view), # <pk> - primary key, <str:text> - string, <slug> - slug
+]
