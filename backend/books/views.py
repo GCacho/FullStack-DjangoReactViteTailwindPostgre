a2137c02bde6de32ed6d1c77d9eb4cc278d1e075
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2023-11-02 20:06:19 -0600

    Styling the book page (forms and book listing).

--- backend/books/views.py
+++ backend/books/views.py
@@ -6,16 +6,16 @@ from django.urls import reverse, reverse_lazy
 # Create your views here.
 
 class BookTitleListView(FormView, ListView):
-    # model = BookTitle
+    model = BookTitle
     # queryset = BookTitle.objects.all()[:3] # Just send the first 3 objects
     # queryset = BookTitle.objects.all().order_by('-created') # Reverse order
     template_name = 'books/main.html'
     context_object_name = 'qs'
     form_class = BookTitleForm
 
-    def get_queryset(self): # A QuerySet find all books that starts with an 'L'
-        parameter = 'L'
-        return BookTitle.objects.filter(title__startswith=parameter)
+    # def get_queryset(self): # A QuerySet find all books that starts with an 'L'
+    #     parameter = 'L'
+    #     return BookTitle.objects.filter(title__startswith=parameter)
     
     def get_success_url(self): # To send the form to the DB with the correct URL
         return self.request.path
