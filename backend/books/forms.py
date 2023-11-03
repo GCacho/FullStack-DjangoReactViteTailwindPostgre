c7ab68a7bf18c866947945932e0db122e05e9920
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2023-11-02 18:58:46 -0600

    First Form created with Django

---
+++ backend/books/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import BookTitle
+
+class BookTitleForm(forms.ModelForm):
+    class Meta:
+        model = BookTitle
+        fields = ('title', 'publisher', 'author')
+        
--- backend/books/views.py
+++ backend/books/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from .models import BookTitle
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView
+from .forms import BookTitleForm
+from django.urls import reverse, reverse_lazy
 # Create your views here.
 
-class BookTitleListView(ListView):
+class BookTitleListView(FormView, ListView):
     # model = BookTitle
     # queryset = BookTitle.objects.all()[:3] # Just send the first 3 objects
     # queryset = BookTitle.objects.all().order_by('-created') # Reverse order
     template_name = 'books/main.html'
     context_object_name = 'qs'
+    form_class = BookTitleForm
 
-    def get_queryset(self):
-        parameter = 'S'
+    def get_queryset(self): # A QuerySet
+        parameter = 'L'
         return BookTitle.objects.filter(title__startswith=parameter)
+    
+    def get_success_url(self): # To send the form to the DB with the correct URL
+        return self.request.path
+    
+    def form_valid(self, form): # To save the form data into the DB
+        form.save()
+        return super().form_valid(form)
     
 
 # -------------------------------------------------------------------------------------------------
